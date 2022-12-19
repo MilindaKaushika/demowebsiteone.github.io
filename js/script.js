7ac72e372f37d6192176ba4a01730fb0240d308f
MilindaKaushika <47909719+MilindaKaushika@users.noreply.github.com>
2022-12-18 18:32:08 -0800

    add mobile menubar

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
+// Mobile responsive naverbar sticky
 const header = document.querySelector("header");
 window.addEventListener ("scroll", function(){
     header.classList.toggle ("sticky", window.scrollY > 100);
 });
+
+// Mobile Resposive naver bar javascript
+let menu = document.querySelector('#menu-icon');
+let naverbar = document.querySelector('.naverbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('bx-x');
+    naverbar.classList.toggle('open');
+}
